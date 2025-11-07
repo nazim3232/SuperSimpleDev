e25467796d08a443f957b52bdca4ecdf3839e84f
nazim32 <chowdhurynazim32@gmail.com>
2025-11-07 21:38:10 +0600

    This is all about the object and way of creating it's in the differnet way .

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 *   This is the revision of the javascript.
-*   
+  
--- javascript/first.js
+++ javascript/first.js
@@ -1,1 +1,41 @@
-console.log("This is the first folder in the javascript.");
+function obj(name , age , gender, bloodGroup)
+{
+  this.name = name ;
+  this.age = age ;
+  this.gender = gender;
+  this.bloodGroup = bloodGroup;
+  this.showDetails=  function()
+  {
+    console.log(`${this.name} , ${this.age} , ${this.gender}, ${this.bloodGroup}`);
+  }
+}
+
+let obj1 = new obj("Nazim Chowdhury", 23, "male", "A+");
+obj1.showDetails();
+
+//this is the another object in this case;
+obj.prototype.showAAge = function()
+{
+  console.log(this.age);
+}
+obj1.showAAge();
+//this is called the constractor function .
+//this is called the factrory function .
+//this is calll the direct object .
+function factory(name , age ,salary, gender)
+{
+  return {
+    name, 
+    age, 
+    salary , 
+    gender, 
+    showDetails: function()
+    {
+      console.log(`${this.name}, ${this.age}, ${this.salary}, ${this.gender} and over all details . `);
+    }
+  }
+}
+
+
+let  p = factory("Nazim Chowdhury",  23, 23000, "male");
+p.showDetails();
