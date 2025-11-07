7e276f061d9c33886686357f86d3695df709e01e
nazim32 <chowdhurynazim32@gmail.com>
2025-11-08 02:56:21 +0600

    This is the use of the this is the js .

---
+++ thisoo.js
@@ -0,0 +1,46 @@
+let uer ={
+    name : "Nazim Chowdhury",
+    age : 23,
+    roll: 23,
+    getData: function ()
+    {
+        console.log(this);
+        console.log("This is the aother div inside the div of the class ")
+          function insider()
+          {
+            //console.log(this)
+          }
+
+          console.log("This is called the insider of  the class ;")
+          insider();
+    }
+}
+
+
+uer.getData();
+
+class mother 
+{
+
+    _name ;
+    _age ;
+    _profession;
+    constructor(n , a , g)
+    {
+        this._name = n;
+        this._age = a;
+        this._profession = g;
+    }
+
+    getData()
+    {
+        console.log(`My mother name is : ${this._name } .`);
+        console.log(`This is the age of the name : ${this._age} .`);
+        console.log(`She is a :${this._age}`);
+    }
+}
+
+let u = new mother("Naim Chowdhury", 23, "teacher");
+u.getData();
+
+//this is the name of the another mother and this is not the another father of this country. 
